b7fd314284ded33463ba4bb936c406c38f20e102
himuselefu-ibraah <ibrahimchristopher004@gmail.com>
2025-03-26 13:24:31 +0300

    initial commit

---
+++ src/constructor.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var dog1 =Dog ("Billy","Chiwawa","Brown")
+    var dog2 =Dog ("Mike","Kienyenji","Black")
+    var dog3 =Dog ("Joseph","German Shepherd","Black")
+    var dog4 =Dog ("ian","Siberian Husky","Black")
+
+    println(dog4.name)
+}
+
+class Dog (var name:String, var breed:String, var colour:String) {
+
+}
